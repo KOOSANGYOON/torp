d40adcd6f3cb7754616652cc0e904febed851920
KOOSANGYOON <nevergiveup936@naver.com>
2018-05-21 16:17:59 +0900

    [START PROJECT] Edit project location.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (id, user_id, password, name, deleted) values (1, 'javajigi', 'test', '자바지기', false);
+INSERT INTO user (id, user_id, password, name, deleted) values (2, 'sanjigi', 'test', '산지기', false);
+INSERT INTO user (id, user_id, password, name, deleted) values (3, 'koo', 'test', '구상윤', false);
+INSERT INTO user (id, user_id, password, name, deleted) values (4, 'pobi', 'test', '박재성교수님', false);
